5418e914cc9b91de50b96759e96a6b3d274a859d
emiliano <e.dimascolo2024@gmail.com>
2025-07-15 12:39:07 +0200

    Add Login Logout Dashboard and Register

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 import express from "express";
 import cors from "cors";
+import { findUser, addUser } from "./user.js";
+import bcrypt from "bcryptjs";
+import session from "express-session";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
+const sessionSecret = process.env.SESSION_SECRET;
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(session({
+    secret: sessionSecret,
+    resave: false,
+    saveUninitialized: true
+}));
+
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const existingUser = await findUser(username);
+    if (existingUser) return res.status(400).send('User already exists');
+    await addUser(username, password);
+    res.send('User registered');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await findUser(username);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).send('Invalid credentials');
+    }
+
+    req.session.user = user.username;
+    res.send('Logged in');
+});
+
+app.get('/dashboard', (req, res) => {
+    if (!req.session.user) return res.status(401).send('Not logged in');
+    res.send(`Hello, ${req.session.user}`);
+});
+
+app.post('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.send('Logged out');
+    });
+});
+
+
+
 
 app.get("/", (req, res) => {
   res.json("Success!");
